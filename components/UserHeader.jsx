a57b8b04d14472f7d33fea0d21bd4aee31ac0f62
Niranjan <niranjanbalurgi@gmail.com>
2025-09-01 15:48:55 +0530

    Create UserHeader.jsx

---
+++ components/UserHeader.jsx
@@ -0,0 +1,63 @@
+function AdminHeader() {
+    return(
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+     
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">
+            Home
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/add">
+            Add Emp
+          </a>
+        </li>
+
+        {/* <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/detail">
+            Detail Emp
+          </a>
+        </li> */}
+
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/userleave">
+            Add Leave
+          </a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/userview">
+            View Leaves
+          </a>
+        </li>
+        
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">
+            Logout
+          </a>
+        </li>
+        
+      </ul>
+      
+    
+    </div>
+  </div>
+</nav>
+
+    )
+}
+
+export default  AdminHeader;
