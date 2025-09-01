02844bee3b0821008843454a474ebb9e919356f7
Niranjan <niranjanbalurgi@gmail.com>
2025-09-01 15:50:25 +0530

    Create AdminLogin.jsx

---
+++ pages/admin/AdminLogin.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function AdminLogin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (username === "admin" && password === "admin") {
+      alert("Login successful!");
+      navigate("/viewemp"); 
+    } else {
+      alert("Invalid admin credentials");
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <div className="card shadow-lg rounded-4">
+            <div className="card-header text-center bg-primary text-white">
+              <h4>Admin Login</h4>
+            </div>
+            <div className="card-body">
+              <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                  <label className="form-label">Username</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter username"
+                    required
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    required
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="d-grid">
+                  <button type="submit" className="btn btn-primary">
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+            {/* <div className="card-footer text-muted text-center">
+              Default: admin / admin123
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminLogin;
